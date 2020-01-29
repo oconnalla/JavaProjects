91f5a37afea685e2cd431e12db7a836f386803e1
oconnalla <oconnalla@gmail.com>
2020-01-28 19:20:31 -0800

    Added assignment 4 which concates a string a set number of times

---
+++ eclipse-workspace/ch4/src/ch4/Repl.java
@@ -0,0 +1,24 @@
+//*******************************************************************
+
+//The class or method name
+//
+//A program for returning a string that concatenates with itself based on a set number of times entered as an argument. 
+//By: Ariel OConnall
+//Date 1/28/20
+//*******************************************************************
+package ch4;
+
+class Repl {
+public static void main(String[] args) {
+  repl("hello", 3);
+  repl("Ariel", 0);
+  repl("Ariel", 6);
+}
+public static void repl(String s, int times){
+  String newS = "";
+  for(int i = 0; i< times; i++){
+    newS += s;
+  }
+System.out.println(newS);
+}
+}
