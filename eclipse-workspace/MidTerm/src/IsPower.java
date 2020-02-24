159a67b087bf372faa6ac88091555371ac153b0f
oconnalla <oconnalla@gmail.com>
2020-02-24 14:38:28 -0800

    added testing to midterm

--- eclipse-workspace/MidTerm/src/IsPower.java
+++ eclipse-workspace/MidTerm/src/IsPower.java
@@ -1,7 +1,8 @@
 
 public class IsPower {
 	public static void main(String[] args){
-		System.out.print(isPowerOfTwo(6));
+		System.out.println(isPowerOfTwo(6));
+		System.out.println(isPowerOfTwo(8));
 	}
 	public static boolean isPowerOfTwo(int n){
 		boolean checker = false;
