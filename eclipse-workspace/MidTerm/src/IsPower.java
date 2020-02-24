4c68ec41dc99c4f8261c79af89508b99ba93098b
oconnalla <oconnalla@gmail.com>
2020-02-24 14:56:22 -0800

    added print lines to show what is happening in isPower()

--- eclipse-workspace/MidTerm/src/IsPower.java
+++ eclipse-workspace/MidTerm/src/IsPower.java
@@ -9,11 +9,16 @@ public class IsPower {
 		if(n <= 0 ) {
 			checker = false;
 		} else {
+			//compare checks the answer when a power is run to see if it goes above n. 
+			//If it's value is above n but never equals n then it's not a power of 2. 
+			//counter is just an an incrementer.
 			double compare = 0;
 			int counter = 0;
-			while( compare < n) {
+			while(compare < n) {
 				compare = Math.pow(2, counter);
-				counter ++;
+				System.out.println("2 to the " + counter + " (counter) equals " + 
+				(String.format("%f", compare).split("\\.")[0]) + " (compare).");
+				counter ++;		
 			}
 			if(compare == n ) {
 			checker = true;
